c5132817f3771acb6f2ed3d5c6ff4a96230d8647
RevolvingDork <chris@hypercombofinish.com>
2012-03-03 11:09:55 -0500

    Initical commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Winnar::Application.config.secret_token = '682271ea0d91f4f86dd79e1dda859a13ad16102d56e142000887df6f35eb2c21c8c7a25973617c0eb403c88c238d2ae9df1b18264394d1822c111949157ae024'
